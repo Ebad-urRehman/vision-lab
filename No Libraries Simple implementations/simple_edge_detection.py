c78464a65e4603abee5a244508f9fc1ce6d716a7
Ebad-urRehman <ebadinfalltraders@gmail.com>
2025-10-10 07:19:36 +0500

    hor + ver edge detector added

--- No Libraries Simple implementations/simple_edge_detection.py
+++ No Libraries Simple implementations/simple_edge_detection.py
@@ -21,6 +21,8 @@ cv.waitKey(0)
 What Kernel for edge detection does : Find difference between 
 intensities of neigbouring pixels and thus making an edge for more different pixels
 """
+
+"""Horizontal edge detection"""
 # simplest horizontal kernel
 kernel = np.array([-1, 1])
 
@@ -31,21 +33,79 @@ rows = image.shape[0]
 cols = image.shape[1]
 
 # creating a matrix for output i.e. 3x2
-output = np.zeros((rows, cols - 1), dtype=np.int32)
+output_horizontal = np.zeros((rows, cols - 1), dtype=np.int32)
 
 # goes through 3x3 grayscale image matrix in 1x2 window
 for row in range(rows):
     for col in range(cols - 1):
         # applying convolution
-        output[row, col] = image[row, col] * kernel[0] + image[row, col + 1] * kernel[1]
+        output_horizontal[row, col] = image[row, col] * kernel[0] + image[row, col + 1] * kernel[1]
+
+# output
+output_display = np.clip(output_horizontal, 0, 255).astype(np.uint8)
+
+# using np.abs
+# output_display = np.clip(np.abs(output_horizontal), 0, 255).astype(np.uint8) # more accurate but noisy edges
+
+
+# show edges image
+cv.imshow("Edges Image Horizontal", np.array(output_display))
+cv.waitKey(0)
+
+cv.imwrite("No Libraries Simple implementations/outputs/simple_horizontal_edge_detection.png", output_display)
+
+# -------------------------------------              ---------------------------------
+
+"""Vertical edge detection"""
+# simplest vertical kernel
+kernel = np.array([[-1], [1]])
+
+# output kernel matrix
+output = []
+
+rows = image.shape[0]
+cols = image.shape[1]
+
+# creating a matrix for output i.e. 3x2
+output_vertical = np.zeros((rows-1, cols), dtype=np.int32)
+
+# goes through 3x3 grayscale image matrix in 1x2 window
+for row in range(rows - 1):
+    for col in range(cols):
+        # applying convolution
+        output_vertical[row, col] = image[row, col] * kernel[0] + image[row + 1, col] * kernel[1]
+
+# output
+output_display = np.clip(output_vertical, 0, 255).astype(np.uint8)
+
+# more accuracte + noisy edges for this task
+# output_display = np.clip(np.abs(output_vertical), 0, 255).astype(np.uint8)
+
+# show edges image
+cv.imshow("Edges Image Vertical", np.array(output_display))
+cv.waitKey(0)
+
+cv.imwrite("No Libraries Simple implementations/outputs/simple_vertical_edge_detection.png", output_display)
+
+
+#----------------------------------------Both combined---------------------
+
+# shapes of output_hor and ouput_ver are diff 
+# making them same size
+min_rows = min(output_horizontal.shape[0], output_vertical.shape[0])
+min_cols = min(output_horizontal.shape[1], output_vertical.shape[1])
+
+output_horizontal = output_horizontal[:min_rows, :min_cols]
+output_vertical = output_vertical[:min_rows, :min_cols]
+
+output = np.abs(output_horizontal) + np.abs(output_vertical)
 
 # output
 output_display = np.clip(output, 0, 255).astype(np.uint8)
 
-# Print result
-for r in output:
-    print(r)
-
 # show edges image
-cv.imshow("Edges Image", np.array(output_display))
+cv.imshow("Edges Image Added", np.array(output_display))
 cv.waitKey(0)
+
+cv.imwrite("No Libraries Simple implementations/outputs/simple_hor_ver_added_edge_detection.png", output_display)
+
